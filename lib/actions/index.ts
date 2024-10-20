6eeb8e74ba3ce692873d80c22cf650a1f46e16b6
Navindu <dahanayakenw.20@uom.lk>
2024-10-20 16:51:45 +0530

    connectToDB implemented

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,16 +1,17 @@
 "use server";
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
    if (!productUrl) return;
 
    try {
-        const scrapedProduct = await scrapeAmazonProduct(productUrl);
+      connectToDB();
+      const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
-        if(!scrapedProduct) return;
-   } catch (error:any) {
-    throw new Error(`Failed to create/update productL ${error.message}`)
+      if (!scrapedProduct) return;
+   } catch (error: any) {
+      throw new Error(`Failed to create/update productL ${error.message}`);
    }
 }
-
