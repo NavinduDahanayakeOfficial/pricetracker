0de16a67f1caecfca67716599e4ddfa78d38f994
Navindu <dahanayakenw.20@uom.lk>
2024-10-21 23:14:27 +0530

    created the Product model for DB

---
+++ lib/models/product.model.ts
@@ -0,0 +1,39 @@
+import { PriceHistoryItems } from "@/types";
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+   {
+      url: { type: String, required: true, unique: true },
+      title: { type: String, required: true },
+      currency: { type: String, required: true },
+      image: { type: String, required: true },
+      currentPrice: { type: Number, required: true },
+      originalPrice: { type: Number, required: true },
+      PriceHistoryItems: [
+         {
+            price: { type: Number, required: true },
+            date: { type: Date, default: Date },
+         },
+      ],
+      highestPrice: Number,
+      lowestPrice: Number,
+      averagePrice: Number,
+      discountRate: Number,
+      description: String,
+      category: String,
+      reviewCount: Number,
+      stars: Number,
+      isOutOfStock: { type: Boolean, default: false },
+      users: [
+         {
+            email: { type: String, required: true },
+         },
+      ],
+      default: [],
+   },
+   { timestamps: true }
+);
+
+const Product = mongoose.models.Products || mongoose.model("Product", productSchema);
+
+export default Product;
