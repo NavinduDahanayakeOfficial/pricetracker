2e9f41665e9d3fde3325e1a82c6b05658927094b
Navindu <dahanayakenw.20@uom.lk>
2024-11-16 07:06:12 +0530

    implemented the price track modal using headlessui/react

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
 import { getProductById, getSimilarProducts } from "@/lib/actions";
@@ -164,7 +165,7 @@ const page = async ({ params: { id } }: Props) => {
                      />
                   </div>
                </div>
-               Modal
+               <Modal />
             </div>
          </div>
 
